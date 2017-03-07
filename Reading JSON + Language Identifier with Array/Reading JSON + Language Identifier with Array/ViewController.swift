c277eba70acaad3b79ed218a8e7f27524d655c88
PancakesAtBreakfast <amsterdamway@gmail.com>
2017-03-07 22:52:27 +0200

    Flower names are displayed in the table view according to the language on device (English, Turkish, Hebrew)

--- Reading JSON + Language Identifier with Array/Reading JSON + Language Identifier with Array/ViewController.swift
+++ Reading JSON + Language Identifier with Array/Reading JSON + Language Identifier with Array/ViewController.swift
@@ -8,11 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var dataList:NSArray!
+    var selectedCellNumberFromDataList:NSDictionary!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        readJSON()
+    }
+    
+    func readJSON(){
+        
+        let languageId = NSLocale.current.languageCode!
+        let jsonFile:String!
+        let jsonData:Data!
+        
+        // Checks if the device's language is not English
+        if languageId != "en"{
+            // If it's not English we'll use languageData json file
+            jsonFile = Bundle.main.path(forResource: "languageData", ofType: "json")
+            jsonData = try? Data(contentsOf: URL(fileURLWithPath: jsonFile!))
+            
+            do {
+                let readableJSON = try JSONSerialization.jsonObject(with: jsonData!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                
+                // translate to dictionary
+                let translateDict = readableJSON["translate to"] as! NSDictionary
+                // Language dictionary according to languageId
+                let languageDict = translateDict[languageId] as! NSDictionary
+                self.dataList = languageDict["data"] as! NSArray
+                print(self.dataList)
+                
+            }catch{
+                print("Error reading json file")
+            }
+            
+        }else {
+            // If it's English we'll use flowersData json file
+            jsonFile = Bundle.main.path(forResource: "flowersData", ofType: "json")
+            jsonData = try? Data(contentsOf: URL(fileURLWithPath: jsonFile!))
+            
+            do {
+                let readableJSON = try JSONSerialization.jsonObject(with: jsonData!, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                
+                self.dataList = readableJSON["data"] as! NSArray
+                print(self.dataList)
+                
+            }catch{
+                print("Error reading json file")
+            }
+            
+        }
+    }
+
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if dataList != nil{
+            return dataList.count
+        }
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let item = dataList[indexPath.row] as! NSDictionary
+        cell.textLabel?.text = item.value(forKey: "name") as! String?
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
